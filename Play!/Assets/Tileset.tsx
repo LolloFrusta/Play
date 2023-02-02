cd8e5fa62fed0b1178cb7bbb9169961238dfc511
Lorenzo <101564222+LolloFrusta@users.noreply.github.com>
2023-02-02 01:59:21 +0100

    Add Assets

---
+++ Play!/Assets/Tileset.tsx
@@ -0,0 +1,334 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Tileset" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="480" columns="16">
+ <image source="../../../Play_Beta/Play_Beta/Assets/Tiled/cityTile.jpg" width="288" height="540"/>
+ <tile id="151">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="152">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="153">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="167">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="169">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="170">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="171">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="181">
+  <properties>
+   <property name="IsOpen" type="bool" value="true"/>
+   <property name="collidable" type="bool" value="false"/>
+   <property name="cost" type="int" value="0"/>
+   <property name="interactable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="183">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="184">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="185">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="186">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="187">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="196">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="197">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="198">
+  <properties>
+   <property name="IsOpen" type="bool" value="true"/>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+   <property name="interactable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="199">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="200">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="201">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="202">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="203">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="212">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="213">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="214">
+  <properties>
+   <property name="IsOpen" type="bool" value="true"/>
+   <property name="collidable" type="bool" value="false"/>
+   <property name="cost" type="int" value="0"/>
+   <property name="interactable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="215">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="218">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="219">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="228">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="229">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="230">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="231">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="232">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="233">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="234">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="240">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="241">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="242">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="256">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="258">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="267">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="272">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="273">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="274">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="275">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="276">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="277">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="278">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="283">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="456">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="457">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="458">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="459">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="460">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="461">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="cost" type="int" value="200"/>
+  </properties>
+ </tile>
+</tileset>
